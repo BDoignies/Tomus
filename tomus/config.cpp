41c61f6a322144ac7630642816fcff9169c464f6
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-12-13 19:12:32 +0100

    Make sure every word is admissible

--- tomus/config.cpp
+++ tomus/config.cpp
@@ -3,7 +3,10 @@
 void Config::SetWords(const std::vector<std::string>& wds)
 {
     for (const auto& w : wds)
+    {
         words.push_back(w);
+        admissibleWords.insert(w); // Make sure every solution is admissible
+    }
 }
 
 void Config::SetAdmissible(const std::vector<std::string>& wds)
